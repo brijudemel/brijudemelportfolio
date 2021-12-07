2650b36019a58b3eb7f65b26f37c982927e57243
brijudemel <63197998+brijudemel@users.noreply.github.com>
2021-12-07 21:01:32 +0530

    Update ProjectList.js

--- src/components/projectList/ProjectList.js
+++ src/components/projectList/ProjectList.js
@@ -8,11 +8,9 @@ function ProjectList() {
     return (
         <div className="pl">
             <div className="pl-texts">
-                <h1 className="pl-title">Create & inspire. It's Lama</h1>
+                <h1 className="pl-title">Here comes interesting stuffs</h1>
                 <p className="pl-desc">
-                Lama is a creative portfolio that your work has been waiting for.
-                Beautiful homes, stunning portfolio styles & a whole lot more awaits
-                inside.
+                    A few of my projects of different technologies with source code. Check it out!!
                 </p>
             </div>
             <div className="pl-list">
